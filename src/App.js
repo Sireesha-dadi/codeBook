5afc1668671e41e3f59bef1c5c350c987fe2a56a
sireesha-dadi <sireeshaibm@gmail.com>
2025-01-03 14:52:35 +0530

    CodeBook Deployment

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { Footer, Header } from './components';
+import { AllRoutes } from './routes/AllRoutes';
+
+
+
+function App() {
+  return (
+    <div className="App dark:bg-darkbg">
+      <Header/>
+      <AllRoutes />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
